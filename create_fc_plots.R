0c1ca9e1c8669d9d1cd8cdb829418fa699fde13f
Morgan Rozman <morganrozman@gmail.com>
2020-04-10 10:45:04 -0500

    Update analyst files

--- create_fc_plots.R
+++ create_fc_plots.R
@@ -2,14 +2,18 @@ library("ggplot2")
 library("ggpubr")
 
 # load limma data
-leflu_data <- read.csv("limma_results_leflunomide.csv", as.is=TRUE)
-flucon_data <- read.csv("limma_results_fluconazole.csv", as.is=TRUE)
-ifos_data <- read.csv("limma_results_ifosfamide.csv", as.is=TRUE)
+leflu_data <- read.csv("limma_results_leflunomide_new.csv", as.is=TRUE)
+flucon_data <- read.csv("limma_results_fluconazole_new.csv", as.is=TRUE)
+ifos_data <- read.csv("limma_results_ifosfamide_new.csv", as.is=TRUE)
 
 # filter for adj.P.Val < 0.05
 leflu_significant <- leflu_data[leflu_data$adj.P.Val < 0.05,]
 flucon_significant <- flucon_data[flucon_data$adj.P.Val < 0.05,]
 ifos_significant <- ifos_data[ifos_data$adj.P.Val < 0.05,]
+
+leflu_significant <- leflu_data[leflu_data$P.Value < 0.05,]
+flucon_significant <- flucon_data[flucon_data$P.Value < 0.05,]
+ifos_significant <- ifos_data[ifos_data$P.Value < 0.05,]
 
 # create histograms
 leflu_hist <- ggplot(leflu_significant, aes(x=logFC)) + geom_histogram(binwidth = 0.2, color="black", fill="#66e0ff") + labs(title="Leflunomide", x="Log Fold Change", y="count") + theme(plot.title = element_text(hjust = 0.5), axis.title.x=element_text(size=8), axis.title.y=element_text(size=8))
@@ -22,7 +26,7 @@ all_hist <- ggarrange(leflu_hist, flucon_hist, ifos_hist,
                       ncol = 3, nrow = 1)
 
 # save to a png
-ggsave("logfc_hist.png", plot = all_hist, width=6.5, height=2.5, units="in")
+ggsave("logfc_hist_new.png", plot = all_hist, width=6.5, height=2.5, units="in")
 
 
 ## create volcano plots using the unfiltered data
@@ -33,9 +37,9 @@ flucon_label <- flucon_data[(flucon_data$logFC > 1.5 | flucon_data$logFC < -1.5)
 ifos_label <- ifos_data[(ifos_data$logFC > 1.5 | ifos_data$logFC < -1.5) & (ifos_data$P.Value < 0.05),]
 
 # get top 3 ids to highlight
-leflu_highlight <- leflu_data[which(leflu_data$X %in% c("1370269_at", "1392946_at", "1395403_at")),]
-flucon_highlight <- flucon_data[which(flucon_data$X %in% c("1371076_at", "1387316_at", "1395403_at")),]
-ifos_highlight <- ifos_data[which(ifos_data$X %in% c("1370355_at", "1369864_a_at", "1387874_at")),]
+leflu_highlight <- leflu_data[which(leflu_data$X %in% c("1370269_at", "1392946_at", "1373810_at")),]
+flucon_highlight <- flucon_data[which(flucon_data$X %in% c("1377014_at", "1371076_at", "1390255_at")),]
+ifos_highlight <- ifos_data[which(ifos_data$X %in% c("1370355_at")),]
 
 # plot settings for all 3 subplots
 v_lines <- list(geom_vline(xintercept = 1.5, col = "red", linetype = "dotted", size = 0.5), geom_vline(xintercept = -1.5, col = "red", linetype = "dotted", size = 0.5), geom_hline(yintercept = -log10(0.05), col = "red", linetype = "dotted", size = 0.5))
@@ -71,4 +75,4 @@ all_scat <- ggarrange(leflu_scat, flucon_scat, ifos_scat,
                       ncol = 3, nrow = 1)
 
 # save to a png
-ggsave("logfc_scat_volcano_notsig.png", plot = all_scat, width=6.5, height=2.5, units="in")
+ggsave("logfc_scat_volcano_notsig_new.png", plot = all_scat, width=6.5, height=2.5, units="in")
--- run_limma.R
+++ run_limma.R
@@ -1,3 +1,5 @@
+# try re-doing limma for just the matching controls
+
 library("limma")
 
 # sample info dataframe with array_id and chemical columns
@@ -5,20 +7,20 @@ samples <- read.csv('/project/bf528/project_3/groups/group_3_mic_info.csv',as.is
 
 # the full RMA normalized matrix of all experiments
 rma <- read.table('/projectnb/bf528/project_3/samples/liver-normalization-rma.txt',
-  sep='\t',
-  as.is=TRUE,
-  header=TRUE,
-  row.names=1,
+                  sep='\t',
+                  as.is=TRUE,
+                  header=TRUE,
+                  row.names=1,
 )
 
 ## LEFLUNOMIDE
 # subset the full expression matrix to just those in this comparison
-rma.subset.leflu <- rma[paste0('X',samples$array_id[samples$chemical=='LEFLUNOMIDE' | samples$chemical=='Control'])]
+rma.subset.leflu <- rma[paste0('X',samples$array_id[samples$chemical=='LEFLUNOMIDE' | (samples$chemical=='Control' & samples$vehicle=='CORN_OIL_100_%')])]
 
 # construct a design matrix modeling treatment vs control for use by limma
 design <- model.matrix(
   ~factor(
-    samples$chemical,
+    samples$chemical[samples$vehicle=='CORN_OIL_100_%'],
     levels=c('Control','LEFLUNOMIDE')
   )
 )
@@ -31,18 +33,20 @@ t <- topTable(fit, coef=2, n=nrow(rma.subset.leflu), adjust='BH')
 
 # filter for unadjusted p-value < 0.05
 t.filtered <- subset(t, adj.P.Val < 0.05)
+t.filtered <- subset(t, P.Value < 0.05)
 # write out the results to file
-write.csv(t,'limma_results_leflunomide.csv')
+write.csv(t,'limma_results_leflunomide_new.csv')
+
 
 
 ## FLUCONAZOLE
 # subset the full expression matrix to just those in this comparison
-rma.subset <- rma[paste0('X',samples$array_id[samples$chemical=='FLUCONAZOLE' | samples$chemical=='Control'])]
+rma.subset <- rma[paste0('X',samples$array_id[samples$chemical=='FLUCONAZOLE' | (samples$chemical=='Control' & samples$vehicle=='CORN_OIL_100_%')])]
 
 # construct a design matrix modeling treatment vs control for use by limma
 design <- model.matrix(
   ~factor(
-    samples$chemical,
+    samples$chemical[samples$vehicle=='CORN_OIL_100_%'],
     levels=c('Control','FLUCONAZOLE')
   )
 )
@@ -55,18 +59,19 @@ t <- topTable(fit, coef=2, n=nrow(rma.subset), adjust='BH')
 
 # filter for unadjusted p-value < 0.05
 t.filtered <- subset(t, adj.P.Val < 0.05)
+t.filtered <- subset(t, P.Value < 0.05)
 # write out the results to file
-write.csv(t,'limma_results_fluconazole.csv')
+write.csv(t,'limma_results_fluconazole_new.csv')
 
 
 ## IFOSFAMIDE
 # subset the full expression matrix to just those in this comparison
-rma.subset <- rma[paste0('X',samples$array_id[samples$chemical=='IFOSFAMIDE' | samples$chemical=='Control'])]
+rma.subset <- rma[paste0('X',samples$array_id[samples$chemical=='IFOSFAMIDE' | (samples$chemical=='Control' & samples$vehicle=='SALINE_100_%')])]
 
 # construct a design matrix modeling treatment vs control for use by limma
 design <- model.matrix(
   ~factor(
-    samples$chemical,
+    samples$chemical[samples$vehicle=='SALINE_100_%'],
     levels=c('Control','IFOSFAMIDE')
   )
 )
@@ -79,5 +84,6 @@ t <- topTable(fit, coef=2, n=nrow(rma.subset), adjust='BH')
 
 # filter for unadjusted p-value < 0.05
 t.filtered <- subset(t, adj.P.Val < 0.05)
+t.filtered <- subset(t, P.Value < 0.05)
 # write out the results to file
-write.csv(t,'limma_results_ifosfamide.csv')
+write.csv(t,'limma_results_ifosfamide_new.csv')
